21904a3bfffc035d9c4dcaa3e2825224f648b489
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-12 21:23:26 +0900

    add project

---
+++ Task-Management-App/Shared/Task_Management_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_Management_AppApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct Task_Management_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
